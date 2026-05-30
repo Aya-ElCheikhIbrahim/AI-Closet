ea9ec48084628d6603b262ab51aaa9d5127123f6
Nour-El-Dandachi <nourdandachi18@gmail.com>
2026-05-30 17:33:19 +0300

    fixed user model

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -2,36 +2,49 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
-use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
-#[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
-   protected $fillable = [
-    'id',
-    'username',
-    'email',
-    'password',
-    'createdAt',
-];
+    use HasFactory, Notifiable;
 
-protected $hidden = [
-    'password',
-    'remember_token',
-];
+    public $timestamps = false;
 
-protected function casts(): array
-{
-    return [
-        'password' => 'hashed',
-        'createdAt' => 'datetime',
+    protected $fillable = [
+        'id',
+        'username',
+        'email',
+        'password',
+        'createdAt',
     ];
-}
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'password' => 'hashed',
+            'createdAt' => 'datetime',
+        ];
+    }
+
+    public function clothingItems()
+    {
+        return $this->hasMany(ClothingItem::class, 'userId');
+    }
+
+    public function outfits()
+    {
+        return $this->hasMany(Outfit::class, 'userId');
+    }
+
+    public function tags()
+    {
+        return $this->hasMany(Tag::class, 'userId');
+    }
 }
