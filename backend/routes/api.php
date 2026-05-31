2c5dcc37239a0745084d5b99c6f38f0142343e3f
Nour-El-Dandachi <nourdandachi18@gmail.com>
2026-05-31 16:58:00 +0300

    fixed error

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ClothingItemController;
+use App\Http\Controllers\OutfitController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
@@ -9,6 +11,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 });
 Route::get('/allClothing/{id?}', [ClothingItemController::class, 'getAllClothingItems']);
-Route::put('/outfit/{id?}', [OutfitController::class,'updateOutfit']);
+Route::post('/outfit/{id?}', [OutfitController::class,'updateOutfit']);
 Route::post('/outfit/{id?}', [OutfitController::class,'createOutfit']);
 Route::delete('/outfit/{id?}', [OutfitController::class,'deleteOutfit']);
